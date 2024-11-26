9b86045bacf07cba3b6160bfe7b2221e0866bfa0
Anka <runner@Mac-1732588335093.local>
2024-11-26 03:23:48 +0000

    Update Package.swift and add generated LiveViewNativeCore.swift to tracking

--- Package.swift
+++ Package.swift
@@ -8,15 +8,15 @@ import class Foundation.ProcessInfo
 let liveview_native_core_framework: Target
 
 // To release, toggle this to `false`
-let useLocalFramework = true
+let useLocalFramework = false
 if useLocalFramework {
     liveview_native_core_framework = .binaryTarget(
         name: "liveview_native_core",
         path: "./target/uniffi/swift/liveview_native_core.xcframework"
     )
 } else {
-    let releaseTag = "0.4.0-alpha-13"
-    let releaseChecksum = "f3972f4d40732c884c98426b28550376abaff20a3490b73367ad170f1f0bcca9"
+    let releaseTag = "documentation-ci-05"
+    let releaseChecksum = "f7a7d0497b733d35ab0de2c5c49393b7a065a336a8e261c29472b4fedd51b038"
     liveview_native_core_framework = .binaryTarget(
         name: "liveview_native_core",
         url:
